389b1e1ace76d63125a008279d25fa29cbba3724
Quentin Guilloteau <Quentin.Guilloteau@inria.fr>
2023-08-02 13:32:38 +0200

    init

---
+++ flake.nix
@@ -0,0 +1,35 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/23.05";
+    daphne-nix.url = "github:GuilloteauQ/daphne-nix";
+  };
+
+  outputs = { self, nixpkgs, daphne-nix }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+    in
+    {
+
+      devShells.${system} = {
+        default = pkgs.mkShell {
+          buildInputs = [
+            daphne-nix.packages.${system}.daphne
+          ];
+        };
+
+        notes = pkgs.mkShell {
+          buildInputs = [
+            pkgs.emacs
+          ];
+          shellHook = ''
+            ${pkgs.emacs}/bin/emacs -q -l ./.init.el notes.org
+            exit
+          '';
+        };
+      };
+
+    };
+}
