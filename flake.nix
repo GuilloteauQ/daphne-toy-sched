8ee574b87b02b486e9571296f9734b5174d853f4
Quentin Guilloteau <Quentin.Guilloteau@inria.fr>
2023-08-03 16:07:40 +0200

    nixfmt

--- flake.nix
+++ flake.nix
@@ -10,46 +10,37 @@
     let
       system = "x86_64-linux";
       pkgs = import nixpkgs { inherit system; };
-    in
-    {
+    in {
 
       devShells.${system} = {
-        default = pkgs.mkShell {
-          buildInputs = [
-            pkgs.snakemake
-            pkgs.wget
-          ];
-        };
+        default = pkgs.mkShell { buildInputs = [ pkgs.snakemake pkgs.wget ]; };
 
         daphne-shell = pkgs.mkShell {
-          buildInputs = [
-            daphne-nix.packages.${system}.daphne
-          ];
+          buildInputs = [ daphne-nix.packages.${system}.daphne ];
         };
 
         daphne-cst-shell = pkgs.mkShell {
           buildInputs = [
-            (daphne-nix.packages.${system}.daphne.overrideAttrs (finalAttrs: previousAttrs: {
-              patchPhase = ''
-                ${previousAttrs.patchPhase}
-                patch -Np1 -i ${./src/cst_sched.patch}
-              '';
-            }))
+            (daphne-nix.packages.${system}.daphne.overrideAttrs
+              (finalAttrs: previousAttrs: {
+                patchPhase = ''
+                  ${previousAttrs.patchPhase}
+                  patch -Np1 -i ${./src/cst_sched.patch}
+                '';
+              }))
           ];
         };
 
         rshell = pkgs.mkShell {
           buildInputs = [
-            (pkgs.rWrapper.override { packages = with pkgs.rPackages; [
-              tidyverse
-            ]; })
+            (pkgs.rWrapper.override {
+              packages = with pkgs.rPackages; [ tidyverse ];
+            })
           ];
         };
 
         notes = pkgs.mkShell {
-          buildInputs = [
-            pkgs.emacs
-          ];
+          buildInputs = [ pkgs.emacs ];
           shellHook = ''
             ${pkgs.emacs}/bin/emacs -q -l ./.init.el notes.org
             exit
